6185a85c3a32f9969ece6e0daaf3da6ece08c96f
Arsalan Asad <https://gitlab.com/arsalanasad96/python-pre-learning-tasks-published/-/tree/master>
2021-01-06 17:32:58 +0000

    results

---
+++ Runner.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+
+ 
+
+public class Runner {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 8;
+		int result1=num1+num2;
+		int result2=num1-num2;
+		int result3=num1*num2;
+		double result4=(double)num1/num2;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		
+	}
+
+}
